9c3a9c71345e75a72468dd7ad9255918d5b77b0b
thanhpv <thanhpv@vnpt-technology.vn>
2021-05-26 23:48:06 +0700

    test1

--- src/main/java/util/SQLquery.java
+++ src/main/java/util/SQLquery.java
@@ -5,4 +5,5 @@ public class SQLquery {
 	public static final String UPDATE = "update dbo.TEAM set TeamName=?, CoachName=?, NationalName=? where ID=?";
 	public static final String DELETE = "delete from dbo.TEAM where ID=?";
 	public static final String SELECT = "select * from dbo.TEAM";
+	asdasdasd
 }
